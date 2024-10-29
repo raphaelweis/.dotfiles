81be473cb6f1faa37da0ef1a63e834be6d41a785
Raphaël Weis <raphael.weis@outlook.com>
2024-10-29 09:30:27 +0100

    fix(nvim): Removed cursor line and cursor column options.

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,8 +7,6 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.opt.cursorcolumn = true
-vim.opt.cursorline = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
