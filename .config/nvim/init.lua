fd2f59d420d9a88368619eb9c721e22ebd03aca0
Raphaël Weis <raphael.weis.g@gmail.com>
2025-10-28 20:08:50 +0100

    feat: nvim fix formatting config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,247 +35,253 @@ vim.keymap.set("n", "<leader>R", "<CMD>restart<CR>")
 -- Bootstrap lazy.nvim if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugin setup with lazy.nvim
 require("lazy").setup({
-  {
-    "tpope/vim-fugitive",
-    config = function()
-      vim.keymap.set("n", "<leader>;", "<CMD>tab Git<CR>")
-    end
-  },
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>;", "<CMD>tab Git<CR>")
+		end,
+	},
 
-  -- Telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      'nvim-tree/nvim-web-devicons'
-    },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          file_ignore_patterns = { "%.git/" },
-        },
-        pickers = {
-          find_files = {
-            hidden = true,
-          },
-        },
-      })
-      vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
-      vim.keymap.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>")
-    end,
-  },
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = { "%.git/" },
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+			})
+			vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
+			vim.keymap.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>")
+		end,
+	},
 
-  -- Colorscheme
-  {
-    "projekt0n/github-nvim-theme",
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme github_dark_default")
-    end,
-  },
+	-- Colorscheme
+	{
+		"projekt0n/github-nvim-theme",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme github_dark_default")
+		end,
+	},
 
-  -- Gitsigns
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {},
-  },
+	-- Gitsigns
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {},
+	},
 
-  -- Autopairs
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
-  },
+	-- Autopairs
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
 
-  -- lualine
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require('lualine').setup({
-        options = {
-          component_separators = { left = '|', right = '|' },
-          section_separators = { left = '', right = '' },
-          always_show_tabline = false,
-        },
-        tabline = {
-          lualine_a = { { 'tabs', mode = 2 } },
-        },
-      })
-    end,
-  },
+	-- lualine
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					component_separators = { left = "|", right = "|" },
+					section_separators = { left = "", right = "" },
+					always_show_tabline = false,
+				},
+				tabline = {
+					lualine_a = { { "tabs", mode = 2 } },
+				},
+			})
+		end,
+	},
 
-  -- Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        auto_install = true,
-        highlight = { enable = true },
-      })
-    end,
-  },
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+				highlight = { enable = true },
+			})
+		end,
+	},
 
-  -- LSP & Mason
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim"
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          "lua-language-server", "typescript-language-server", "stylua", "prettier"
-        },
-      })
+	-- LSP & Mason
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"lua-language-server",
+					"typescript-language-server",
+					"stylua",
+					"prettier",
+				},
+			})
 
-      -- Lua LS setup
-      vim.lsp.config("lua_ls", {
-        on_init = function(client)
-          if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if
-                path ~= vim.fn.stdpath("config")
-                and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-            then
-              return
-            end
-          end
+			-- Lua LS setup
+			vim.lsp.config("lua_ls", {
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if
+							path ~= vim.fn.stdpath("config")
+							and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+						then
+							return
+						end
+					end
 
-          client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-            runtime = {
-              version = "LuaJIT",
-              path = { "lua/?.lua", "lua/?/init.lua" },
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = { vim.env.VIMRUNTIME },
-            },
-          })
-        end,
-        settings = { Lua = {} },
-      })
-      vim.lsp.enable("lua_ls")
-      vim.lsp.enable("ts_ls")
-    end,
-  },
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							version = "LuaJIT",
+							path = { "lua/?.lua", "lua/?/init.lua" },
+						},
+						workspace = {
+							checkThirdParty = false,
+							library = { vim.env.VIMRUNTIME },
+						},
+					})
+				end,
+				settings = { Lua = {} },
+			})
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("ts_ls")
+		end,
+	},
 
-  -- Conform (formatting)
-  {
-    'stevearc/conform.nvim',
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
-        javascriptreact = { "prettier" },
-        html = { "prettier" },
-        css = { "prettier" },
-        json = { "prettier" },
-        markdown = { "prettier" },
-        yaml = { "prettier" }
-      },
-    },
-    config = function()
-      vim.api.nvim_create_user_command("Format", function(args)
-        local range = nil
-        if args.count ~= -1 then
-          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-          range = {
-            start = { args.line1, 0 },
-            ["end"] = { args.line2, end_line:len() },
-          }
-        end
-        require("conform").format({ async = true, lsp_format = "fallback", range = range })
-      end, { range = true })
-      vim.keymap.set("n", "<leader>fm", "<CMD>Format<CR>")
-    end,
-  },
+	-- Conform (formatting)
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					typescriptreact = { "prettier" },
+					javascriptreact = { "prettier" },
+					html = { "prettier" },
+					css = { "prettier" },
+					json = { "prettier" },
+					markdown = { "prettier" },
+					yaml = { "prettier" },
+				},
+			})
+			vim.api.nvim_create_user_command("Format", function(args)
+				local range = nil
+				if args.count ~= -1 then
+					local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+					range = {
+						start = { args.line1, 0 },
+						["end"] = { args.line2, end_line:len() },
+					}
+				end
+				require("conform").format({ async = true, lsp_format = "fallback", range = range })
+			end, { range = true })
+			vim.keymap.set("n", "<leader>fm", "<CMD>Format<CR>")
+		end,
+	},
 
-  -- nvim-cmp (completion)
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'L3MON4D3/LuaSnip',
-      'rafamadriz/friendly-snippets',
-    },
-    config = function()
-      local cmp = require 'cmp'
+	-- nvim-cmp (completion)
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			local cmp = require("cmp")
 
-      local cmp_kinds = {
-        Text = '  ',
-        Method = '  ',
-        Function = '  ',
-        Constructor = '  ',
-        Field = '  ',
-        Variable = '  ',
-        Class = '  ',
-        Interface = '  ',
-        Module = '  ',
-        Property = '  ',
-        Unit = '  ',
-        Value = '  ',
-        Enum = '  ',
-        Keyword = '  ',
-        Snippet = '  ',
-        Color = '  ',
-        File = '  ',
-        Reference = '  ',
-        Folder = '  ',
-        EnumMember = '  ',
-        Constant = '  ',
-        Struct = '  ',
-        Event = '  ',
-        Operator = '  ',
-        TypeParameter = '  ',
-      }
+			local cmp_kinds = {
+				Text = "  ",
+				Method = "  ",
+				Function = "  ",
+				Constructor = "  ",
+				Field = "  ",
+				Variable = "  ",
+				Class = "  ",
+				Interface = "  ",
+				Module = "  ",
+				Property = "  ",
+				Unit = "  ",
+				Value = "  ",
+				Enum = "  ",
+				Keyword = "  ",
+				Snippet = "  ",
+				Color = "  ",
+				File = "  ",
+				Reference = "  ",
+				Folder = "  ",
+				EnumMember = "  ",
+				Constant = "  ",
+				Struct = "  ",
+				Event = "  ",
+				Operator = "  ",
+				TypeParameter = "  ",
+			}
 
-      cmp.setup({
-        completion = {
-          completeopt = 'menu,menuone,noinsert'
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-          { name = 'buffer' },
-        }),
-        formatting = {
-          format = function(_, vim_item)
-            vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
-            return vim_item
-          end,
-        },
-      })
-    end
-  }
+			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+					{ name = "buffer" },
+				}),
+				formatting = {
+					format = function(_, vim_item)
+						vim_item.kind = (cmp_kinds[vim_item.kind] or "") .. vim_item.kind
+						return vim_item
+					end,
+				},
+			})
+		end,
+	},
 })
