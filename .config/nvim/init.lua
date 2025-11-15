e5f948f1d02c38bdcce1870924e4a561ca8d83bc
Raphaël Weis <raphael.weis.g@gmail.com>
2025-11-15 08:21:33 +0100

    feat: nvim vimtex

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -339,4 +339,11 @@ require("lazy").setup({
 			dependencies_bin = { ["tinymist"] = "tinymist" },
 		},
 	},
+	{
+		"lervag/vimtex",
+		lazy = false, -- we don't want to lazy load VimTeX
+		init = function()
+			vim.g.vimtex_view_method = "sioyek"
+		end,
+	},
 })
