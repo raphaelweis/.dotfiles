23e5d858b4ed35654351668f887717e17488ec11
Raphaël Weis <raphael.weis.g@gmail.com>
2026-01-03 17:58:08 +0100

    feat: typst preview + tmux correct term variable

--- .config/nvim/ftplugin/typst.lua
+++ .config/nvim/ftplugin/typst.lua
@@ -1,3 +1,15 @@
 vim.opt.spell = true
 vim.opt.spelllang = { "en", "fr" }
 vim.opt.textwidth = 80
+
+local MiniDeps = require("mini.deps")
+local add, now = MiniDeps.add, MiniDeps.now
+
+now(function()
+	add({ source = "chomosuke/typst-preview.nvim", checkout = "v1.*" })
+
+	require("typst-preview").setup({
+		open_cmd = "firefox --new-window %s",
+		port = 8000,
+	})
+end)
